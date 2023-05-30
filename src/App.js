82408e3e6ad1832962b07a141e54378ee514f5ef
Rylie Castell <rylie.castell@gmail.com>
2023-05-31 00:22:06 +0200

    add entries section content

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Button from "./components/Button";
 import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
+import EntryList from "./components/EntryList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import Main from "./components/Main";
+import TabBar from "./components/TabBar";
 import Textarea from "./components/Textarea";
 
 export default function App() {
@@ -18,7 +20,10 @@ export default function App() {
           <Textarea />
           <Button type="submit" text="Submit" />
         </EntryForm>
-        <EntriesSection />
+        <EntriesSection>
+          <TabBar />
+          <EntryList />
+        </EntriesSection>
       </Main>
       <Footer />
     </>
